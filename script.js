b3d45efb4eb0567145f0a7cb43d14cefc7441425
rubel04 <alomrana886@gmail.com>
2025-01-17 17:16:22 +0600

    create image to text converter app

---
+++ script.js
@@ -0,0 +1,26 @@
+const imageInput = document.getElementById("fileInput");
+const extractText = document.getElementById("extract-text");
+const images = document.getElementById('images')
+const text = document.getElementById('text')
+const extractImage = () => {
+  const imageFile = imageInput.files[0];
+  if (imageFile) {
+    Tesseract.recognize(imageFile, "eng")
+      .then(({ data }) => {
+        extractText.innerText = data.text;
+      })
+      .catch((err) => {
+        console.error("Error", err);
+        extractText.innerText = ` ${err}`;
+      });
+  } else {
+    extractText.innerHTML = "Please select an image";
+  }
+};
+
+imageInput.addEventListener('change', () => {
+    let imageLink = URL.createObjectURL(imageInput.files[0])
+    images.classList.remove('hidden')
+    text.classList.remove('hidden')
+    images.style.backgroundImage = `url(${imageLink})`
+})
